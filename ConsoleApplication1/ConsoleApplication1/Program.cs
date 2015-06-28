71de4fae0de1f89a7f5d7fdaa4ebd2bf28158b42
Arkadiusz <sitarz.arkadiusz@gmail.com>
2015-06-28 18:19:20 +0200

    pusta metoda wraz z nieudanym testem

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,21 +1,23 @@
 ﻿using System.IO;
 using System;
 
-class Program
-{
-    static void Main()
+namespace Project1{
+    public class Program
     {
+        static void Main()
+        {
 
-        Console.ReadKey();
-    }
+            Console.ReadKey();
+        }
 
 
 
 
-    // method that check if dividedNumber can be divided by numbers from maxDivisor to 1 
-    static bool CheckIfDividable(long dividedNumber, int maxDivisor)
-    {
+        // method that check if dividedNumber can be divided by numbers from maxDivisor to 1 
+        public static bool CheckIfDividable(long dividedNumber, int maxDivisor)
+        {
 
-        return false;
+            return false;
+        }
     }
 }
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            bool b = Program.CheckIfDividable(3, 4);
+            Assert.IsTrue(b);
+
+        }
+    }
+}
