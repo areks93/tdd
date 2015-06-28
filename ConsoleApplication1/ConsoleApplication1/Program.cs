ad625b308f33eb8c703bc8bf7f4163ced03f8139
Arkadiusz <sitarz.arkadiusz@gmail.com>
2015-06-28 18:08:01 +0200

    Pierwsza pusta metoda.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System;
 
-namespace ConsoleApplication1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Tdd");
-            Console.ReadLine();
-        }
+
+        Console.ReadKey();
+    }
+
+
+
+
+    // method that check if dividedNumber can be divided by numbers from maxDivisor to 1 
+    static bool CheckIfDividable(long dividedNumber, int maxDivisor)
+    {
+
+        return false;
     }
 }
